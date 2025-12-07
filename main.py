5bba77165389a1493e935187b6feef255143350f
ivanamol <ivaryd@post.cz>
2025-12-07 10:03:32 +0100

    Připravené jednotlivé funkce, hlavní část funkční, použití argparse

--- main.py
+++ main.py
@@ -1,74 +1,72 @@
-import os
 import argparse
 import csv
 from requests import get
 from bs4 import BeautifulSoup
 
-def zpracuj_odpoved_url(zadana_url: str) -> BeautifulSoup:
-    obsah = get(zadana_url)
-    return BeautifulSoup(obsah.text, features="html.parser")
+def process_answer_url(url: str) -> BeautifulSoup:
+    contens = get(url)
+    return BeautifulSoup(contens.text, features="html.parser")
 
-def seznam_cisel_obci(soup):
-    vsechna_td_cislo = soup.find_all("td", {"class": "cislo"})
-    return [cislo.text for cislo in vsechna_td_cislo]
+def getting_municipalities_numbers(soup):
+    all_td_numbers = soup.find_all("td", {"class": "cislo"})
+    return [number.text for number in all_td_numbers]
 
-def cislo_kraje(zadana_url):
-    return zadana_url.split("&")[-2].split("=")[-1]
+def getting_region_number(url):
+    return url.split("&")[-2].split("=")[-1]
 
-def cislo_okresu(zadana_url):
-    return zadana_url.split("&")[-1].split("=")[-1]
+def getting_district_number(url):
+    return url.split("&")[-1].split("=")[-1]
 
-def url_tezba(zadana_url, soup_zadane_url):
-    kraj = cislo_kraje(zadana_url)
-    okres = cislo_okresu(zadana_url)
-    seznam_obci = seznam_cisel_obci(soup_zadane_url)
-    return [f"https://www.volby.cz/pls/ps2017nss/ps311?xjazyk=CZ&xkraj={kraj}&xobec={obec}&xvyber={okres}" for obec in seznam_obci]
+def url_mining(url, soup_url):
+    region_number = getting_region_number(url)
+    district_number = getting_district_number(url)
+    list_of_municipalities = getting_municipalities_numbers(soup_url)
+    return [f"https://www.volby.cz/pls/ps2017nss/ps311?xjazyk=CZ&xkraj={region_number}&xobec={municipality}&xvyber={district_number}" for municipality in list_of_municipalities]
 
-def ziskani_nazvu_obce(soap):
+def getting_name_of_municipality(soap):
     return soap("h3")[2].get_text(strip=True).split(":")[1].strip()
 
-def ziskani_dat_souhrn(soap):
-    hlavicka = soap.find("table", {"id": "ps311_t1"}).find_all("tr")[2]
-    hodnoty_hlavicek = hlavicka.get_text().strip()
-    return hodnoty_hlavicek.split("\n")
+def getting_summary_data(soap):
+    data_header = soap.find("table", {"id": "ps311_t1"}).find_all("tr")[2]
+    values_in_data_header = data_header.get_text().strip()
+    return values_in_data_header.split("\n")
 
-def ziskani_dat_stran(soap):
-    tabulka = soap.find_all("table", {"class": "table"})
-    vysledky_stran2 = {}
+
+def getting_political_parties_data(soap):
+    table = soap.find_all("table", {"class": "table"})
+    result_of_politicial_parties = {}
     for index in range(1, 3):
-        tabulka_cislo = tabulka[index]
-        vsechny_tr_tabulka2 = tabulka_cislo.find_all("tr")
-        for tr in vsechny_tr_tabulka2[2:]:
-            vsechna_td_v_jednotlivych_tr2 = tr.find_all("td")
-            vysledky_stran2[vsechna_td_v_jednotlivych_tr2[1].get_text()] = vsechna_td_v_jednotlivych_tr2[2].get_text()
-    return vysledky_stran2
+        for tr in table[index].find_all("tr")[2:]:
+            result_of_politicial_parties[tr.find_all("td")[1].get_text()] = tr.find_all("td")[2].get_text()
+    return result_of_politicial_parties
 
-def uloz_do_csv(nazev_souboru, seznam_slovniku):
-    with open(nazev_souboru, mode="w", newline="", encoding="UTF-8") as nove_csv:
-        zahlavi = seznam_slovniku[0].keys()
-        zapisovac = csv.DictWriter(nove_csv, fieldnames=zahlavi)
-        zapisovac.writeheader()
-        for slovnik in seznam_slovniku:
-            zapisovac.writerow(slovnik)
+def save_to_csv(file_name, list_of_dictionaries):
+    with open(file_name, mode="w", newline="", encoding="UTF-8") as new_csv:
+        table_header = list_of_dictionaries[0].keys()
+        writer_csv = csv.DictWriter(new_csv, fieldnames=table_header)
+        writer_csv.writeheader()
+        for dictionary in list_of_dictionaries:
+            writer_csv.writerow(dictionary)
 
-
-zadana_url = "https://www.volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=2&xnumnuts=2101"
 
 def main():
-    odpoved_zadane_url = zpracuj_odpoved_url(zadana_url)
-    cisla_obci = seznam_cisel_obci(odpoved_zadane_url)
-    kompletni_slovnik = {}
-    vysledek = []
-    for jedna_samotinka_url in url_tezba(zadana_url, odpoved_zadane_url):
-        odpoved_url_obce = zpracuj_odpoved_url(jedna_samotinka_url)
-        nazev_obce = ziskani_nazvu_obce(odpoved_url_obce)
-        ziskani_kodu_obce = jedna_samotinka_url.split("&")[-2].split("=")[-1]
-        souhrn = ziskani_dat_souhrn(odpoved_url_obce)
-        strany_slovnik = ziskani_dat_stran(odpoved_url_obce)
-        uvodni_slovnik = {"kód obce": ziskani_kodu_obce, "název obce": nazev_obce, "voliči v seznamu": souhrn[3], "vydané obálky": souhrn[4], "platné hlasy": souhrn[7]}
-        kompletni_slovnik = uvodni_slovnik | strany_slovnik
-        vysledek.append(kompletni_slovnik)
-    uloz_do_csv("volby.csv", vysledek)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--url", type=str)
+    parser.add_argument("--file_name", type=str)
+    args = parser.parse_args()
+    answer_given_url = process_answer_url(args.url)
+    complete_dictionary = {}
+    result = []
+    for single_url in url_mining(args.url, answer_given_url):
+        answer_url_municipality = process_answer_url(single_url)
+        election_summary = getting_summary_data(answer_url_municipality)
+        political_parties_dictionary = getting_political_parties_data(answer_url_municipality)
+        election_summary_dictionary = {"code": single_url.split("&")[-2].split("=")[-1], "location": getting_name_of_municipality(answer_url_municipality), "registered": election_summary[3], "envelopes": election_summary[4], "valid": election_summary[7]}
+        complete_dictionary = election_summary_dictionary | political_parties_dictionary
+        result.append(complete_dictionary)
+    save_to_csv(args.file_name, result)
+
 
 if __name__ == "__main__":
     main()
+
